e069ea456c547874313c9f0b507493bbd761af43
Guillermo Uscudum <68258148+uscudum@users.noreply.github.com>
2023-01-24 11:11:05 -0300

    class Auto, Persona

---
+++ IntroJava/src/Relaciones/Auto.java
@@ -0,0 +1,15 @@
+package Relaciones;
+
+public class Auto {
+    public String marca;
+    public String modelo;
+    public String matricula;
+    public int anio;
+    public Auto(String marca, String modelo, String matricula, int anio){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.matricula = matricula;
+        this.anio = anio;
+    }
+}
+
---
+++ IntroJava/src/Relaciones/Persona.java
@@ -0,0 +1,18 @@
+package Relaciones;
+
+public class Persona {
+
+    public String nombre;
+    public String ci;
+    public boolean mayorEdad;
+    public Auto auto;
+
+    public Persona(String nombre, String ci, boolean mayorEdad, Auto auto){
+        this.nombre = nombre;
+        this.ci = ci;
+        this.mayorEdad = mayorEdad;
+        this.auto = auto;
+    }
+}
+
+
