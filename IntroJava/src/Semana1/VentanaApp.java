260845257a6cf7443526b8291c8b5564d7e12638
guillermo.uscudum <guilleuscudum@gmail.com>
2023-02-11 20:46:27 -0300

    GUI Form
    Example GUI Form which label, textBox and button

--- IntroJava/src/Semana1/VentanaApp.java
+++ IntroJava/src/Semana1/VentanaApp.java
@@ -1,4 +1,32 @@
 package Semana1;
 
-public class VentanaApp {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VentanaApp extends JFrame{
+    private JPanel panelMain;
+    private JLabel lblName;
+    private JTextField txtName;
+    private JButton btnSave;
+
+    public VentanaApp(){
+        setTitle("Welcome");
+        setSize(450,300);
+        setContentPane(panelMain);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        btnSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(btnSave, txtName.getText() + " , hola");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        VentanaApp myFrame = new VentanaApp();
+    }
+
+
 }
